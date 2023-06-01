13d39c734f9b424ff23aa18bf7f5109ec730e380
unknown <jahandarmv@code.edu.az>
2023-06-01 18:30:31 +0400

    Home page html,css done

---
+++ assets/js/home.js
@@ -0,0 +1,110 @@
+
+//product-slider---- Start ----------
+
+const sliders = [...document.querySelectorAll(".slider__container")];
+const sliderControlPrev = [...document.querySelectorAll(".slider__control.prev")];
+const sliderControlNext = [...document.querySelectorAll(".slider__control.next")];
+
+sliders.forEach((slider, i) => {
+  let isDragStart = false,
+    isDragging = false,
+    isSlide = false,
+    prevPageX,
+    prevScrollLeft,
+    positionDiff;
+
+  const sliderItem = slider.querySelector(".slider__item");
+  var isMultislide = (slider.dataset.multislide === 'true');
+
+  sliderControlPrev[i].addEventListener('click', () => {
+    if (isSlide) return;
+    isSlide = true;
+    let slideWidth = isMultislide ? slider.clientWidth : sliderItem.clientWidth;
+    slider.scrollLeft += -slideWidth;
+    setTimeout(function () { isSlide = false; }, 700);
+  });
+
+  sliderControlNext[i].addEventListener('click', () => {
+    if (isSlide) return;
+    isSlide = true;
+    let slideWidth = isMultislide ? slider.clientWidth : sliderItem.clientWidth;
+    slider.scrollLeft += slideWidth;
+    setTimeout(function () { isSlide = false; }, 700);
+  });
+
+  function autoSlide() {
+    if (slider.scrollLeft - (slider.scrollWidth - slider.clientWidth) > -1 || slider.scrollLeft <= 0) return;
+    positionDiff = Math.abs(positionDiff);
+    let slideWidth = isMultislide ? slider.clientWidth : sliderItem.clientWidth;
+    let valDifference = slideWidth - positionDiff;
+    if (slider.scrollLeft > prevScrollLeft) {
+      return slider.scrollLeft += positionDiff > slideWidth / 5 ? valDifference : -positionDiff;
+    }
+    slider.scrollLeft -= positionDiff > slideWidth / 5 ? valDifference : -positionDiff;
+  }
+
+  function dragStart(e) {
+    if (isSlide) return;
+    isSlide = true;
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = slider.scrollLeft;
+    setTimeout(function () { isSlide = false; }, 700);
+  }
+
+  function dragging(e) {
+    if (!isDragStart) return;
+    e.preventDefault();
+    isDragging = true;
+    slider.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    slider.scrollLeft = prevScrollLeft - positionDiff;
+  }
+
+  function dragStop() {
+    isDragStart = false;
+    slider.classList.remove("dragging");
+    if (!isDragging) return;
+    isDragging = false;
+    autoSlide();
+  }
+
+  addEventListener("resize", autoSlide);
+  slider.addEventListener("mousedown", dragStart);
+  slider.addEventListener("touchstart", dragStart);
+  slider.addEventListener("mousemove", dragging);
+  slider.addEventListener("touchmove", dragging);
+  slider.addEventListener("mouseup", dragStop);
+  slider.addEventListener("touchend", dragStop);
+  slider.addEventListener("mouseleave", dragStop);
+});
+
+
+//product Slider ---END -------------
+
+
+
+
+$(document).ready(function () {
+  $('.customer-logos').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 3
+      }
+    }]
+  });
+});
+
