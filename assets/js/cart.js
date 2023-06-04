f35429ce45e03472e39f04e9e3e2db05c30b4fb9
unknown <jahandarmv@code.edu.az>
2023-06-04 20:15:25 +0400

    Home Page responsive done,Blog page done,Contact page done,Cart page done ,Shoes Page edit

---
+++ assets/js/cart.js
@@ -0,0 +1,40 @@
+// SIDEBAR
+let iconx = document.querySelector(".iconx");
+let icon = document.querySelector(".fa-bars");
+let overlay = document.querySelector(".overlay");
+
+icon.onclick = function () {
+  var sidebar = document.querySelector(".content-area");
+  sidebar.style.width = "80%";  
+  sidebar.style.visibility = "visible";
+  sidebar.style.opacity = "1";
+};
+
+iconx.onclick = function () {
+  var sidebar = document.querySelector(".content-area");
+  sidebar.style.width = "0%";
+  sidebar.style.visibility = "hidden";
+  sidebar.style.opacity = "0";
+};
+
+
+//Sticky--menu 
+
+$(function(){
+  "use strict";
+
+  // Sticky menu 
+  var $window = $(window);
+  $window.on('scroll', function () {
+    var scroll = window.scrollY;
+    if (scroll < 200) {
+      $(".down-navbar").removeClass("stick-nav");
+    } else {
+      $(".down-navbar").addClass("stick-nav");
+    }
+  });
+
+
+})
+
+
